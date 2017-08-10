e7dd97ec4ab9771a69401ebe174e91df48117d0c
bon-adachi <adachi@bondesignworks.com>
2017-08-10 19:28:50 +0900

    phpファイルに配列バージョンの記述を追加する。

--- music.php
+++ music.php
@@ -1,4 +1,17 @@
 <?php
-  // echo 'json_encode(array("src"=>"music.mp3"))';
+  // $array = array(
+  //   'music.mp3',
+  //   'music2.mp3',
+  //   'music3.mp3',
+  //   'music4.mp3',
+  //   'music5.mp3',
+  //   'music6.mp3',
+  //   'music7.mp3',
+  //   'music8.mp3',
+  //   'music9.mp3',
+  //   'music10.mp3'
+  // );
+  // $data = json_encode($array);
+  // echo $data;
   echo "music.mp3";
 ?>
