adc69b02462c70f1407eb6b16697a5a5fffe73a1
bon-adachi <adachi@bondesignworks.com>
2017-08-10 19:31:21 +0900

    touchstartではaudio要素を再生できなかったため、clickで実装する。 また、js側で配列を受け取れる記述を追加する。

--- common.js
+++ common.js
@@ -4,6 +4,8 @@ $(function () {
     type: 'GET', // GET or POST
     url: 'music.php', // 取得するファイルpath
     success: function (data) {  //取得成功のとき
+      // res = JSON.parse(data)  //配列を受け取る
+      // console.log(res);
       playMusic(data);
     },
 
@@ -14,15 +16,25 @@ $(function () {
 
   function playMusic($a) {
     var $src = $a;
-    $('#audio').attr('src', $src);  // src属性を動的に追加
+    $('#audio').attr('src', 'music/' + $src);  // src属性を動的に追加
     var $audio = $('#audio').get(0);  // getelementbyid
     $audio.play();
     if ($audio.paused) {
       $('body').append('<p>停止中</p>');
-      $('body').on('tac')
-    } else {
-      $('body').append('<p>再生中</p>');
+      $('div').on('click', function () {
+        $audio.play();
+        setTimeout(function () {
+          $audio.removeAttribute("src");  // src属性を削除
+        }, 10);
+        setTimeout(function () {
+          $audio.pause();  // 任意の秒数後に再生＆読み込みの停止
+          $audio.load();
+          // }, 60000);
+        }, 10000);
+      });
     }
+    else {
+      $('body').append('<p>再生中</p>');
       setTimeout(function () {
         $audio.removeAttribute("src");  // src属性を削除
       }, 10);
@@ -31,6 +43,15 @@ $(function () {
         $audio.load();
         // }, 60000);
       }, 10000);
+    }
+      // setTimeout(function () {
+      //   $audio.removeAttribute("src");  // src属性を削除
+      // }, 10);
+      // setTimeout(function () {
+      //   $audio.pause();  // 任意の秒数後に再生＆読み込みの停止
+      //   $audio.load();
+      //   // }, 60000);
+      // }, 10000);
   }
 
 
